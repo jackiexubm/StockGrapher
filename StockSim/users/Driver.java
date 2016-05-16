b048e8b5d82bfc3fc9c14a2b7b8bfdb09293bafa
Jonathan Mikhaylov <JonathanMikhaylov@Jonathans-MacBook-Pro-2.local>
2016-05-16 14:32:24 -0400

    Finished SignIn. Need to implement password encryptor and finish SignUp

--- StockSim/users/Driver.java
+++ StockSim/users/Driver.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Driver{
 
@@ -6,38 +7,67 @@ public class Driver{
     //writer.println("The first line");
     //writer.println("The second line");
     public static Scanner sc = new Scanner(System.in);
-    
+    public static String[] userArray;
     public static void main(String[] args) {
 	hasAccount();
     }    
 
     public static void hasAccount(){
-	System.out.print("Do you have an account? (yes/no)");
-	if (sc.next().equalsIgnoreCase("no")) {
+	System.out.println("Do you have an account? (yes/no)");
+	String yesNo = sc.next();
+	if (yesNo.equalsIgnoreCase("no")) {
 	    signUp();
-	} else if (sc.next().equalsIgnoreCase("yes")) {
+	} else if (yesNo.equalsIgnoreCase("yes")) {
 	    signIn();
-	} 
-	
+	} 	
     }
     
     public static void signUp() {
-	System.out.print("Enter a username:");
+	System.out.print("Enter a username: ");
 	String username = sc.next();
 	
-	System.out.print("Enter a password:");
+	System.out.print("Enter a password: ");
 	String password = sc.next();
 	
 	User a = new User(username, password);
     }
-    
+
     public static void signIn() {
-	System.out.print("Enter your username:");
-	String usr = sc.next();
-	
-	
+        String fileName = "accounts.csv";
+        String line = null;
+
+        try {
+            FileReader fileReader = new FileReader(fileName); 
+	    BufferedReader bR =  new BufferedReader(fileReader);
+	    
+	    System.out.println("Enter your username: ");
+	    String usr = sc.next();
+
+            while ((line = bR.readLine()) != null) {
+		//line = bR.readLine();
+		userArray = line.split(",");
+		if (userArray[0].equals(usr) ) {
+		    System.out.println("Enter your password: ");
+		    String pass = sc.next();	
+		    if (userArray[1].equals(pass) ) { 
+			System.out.println("WELCOME TO THE STOCK SIMULATOR");
+		    } else {
+			System.out.println("Password does NOT match");
+			    }
+		} else {
+		    System.out.println("Username does NOT exist");
+			}
+            }   
+            bR.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
     }
-    
-    //writer.close();
 
 }
