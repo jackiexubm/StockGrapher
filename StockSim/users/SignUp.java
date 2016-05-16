0c5c0154ede89902aef38108cd8d99bb868f08bd
Jonathan Mikhaylov <JonathanMikhaylov@Jonathans-MacBook-Pro-2.local>
2016-05-16 11:43:11 -0400

    Added toString and fixed Driver. Still have to implement encrypt()

--- StockSim/users/SignUp.java
+++ StockSim/users/SignUp.java
@@ -2,17 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class SignUp{
-    /*
-     * To change this template, choose Tools | Templates
-     * and open the template in the editor.
-     */
-    package commandssvn;
+    //package commandssvn;
  
 //    public static void main(String[] args) throws IOException {
 //	generateCsvFile();
 //	run();
         
     //   }
+    public Scanner sc = new Scanner(system.in);
+
+    public void hasAccount(String[] args){
+        System.out.println("Do you have an account? (yes/no)");
+        if (sc.nextLine().equalsIgnoreCase("no")) {
+            signUp();
+        } else if (sc.nextLine().equalsIgnoreCase("yes")) {
+            signIn();
+        }
+
+    }
+
+    public void signUp() {
+        System.out.print("Enter a username:");
+        String username = sc.next();
+
+        System.out.print("Enter a password:");
+        String password = sc.next();
+
+        User a = new User(username, password);
+    }
+
+    public void signIn() {
+        System.out.print("Enter your username:");
+        String usr = sc.next();
+    }
+
+    //--------------------------------------------------------------------------------------
     public static void run() {
 		        
 	BufferedReader br = null;
@@ -21,8 +45,7 @@ public class SignUp{
 	
 	try {
 	    br = new BufferedReader(new FileReader(csvFile));
-	    
-	    
+	    	    
 	    boolean firstTime=false;
 	    int i=0;
 	    while ((line = br.readLine()) != null) {
@@ -34,7 +57,7 @@ public class SignUp{
 		if(i==4)
 		    {
 			i=0;
-			generateCsvFile(arrayStrings);
+			generateCsvFile();
 		    }
 		
 	    }
@@ -55,26 +78,8 @@ public class SignUp{
         
     }
       
-    private static void generateCsvFile()
-    {
-	try
-	    {
-		FileWriter writer = new FileWriter("accounts.csv",false);
-		writer.append('\n');
-		
-		writer.flush();
-		writer.close();
-	    }
-	catch(IOException e)
-	    {
-		e.printStackTrace();
-	    }
-    }
-    
-    
     private static void fileWrite(String usr) {
-	try
-	    {
+	try {
 		FileWriter writer = new FileWriter("accounts.csv");
 		writer.append(usr+',');
 		writer.append();
@@ -85,26 +90,24 @@ public class SignUp{
 		writer.flush();
 		writer.close();
 	    }
-	
 	catch(IOException e) {
 	    e.printStackTrace();
 	}
 	
     }
 }
-
+/*
     public String encrypt(String password) {
         String hash = "";
         try {
-            MessageDigest md5 = MessageDigest.getInstance("SHA-512");
-            byte [] digest = md5.digest(password.getBytes("UTF-8"));
+	    MessageDigest md5 = MessageDigest.getInstance("SHA-512");
+            byte[] digest = md5.digest(password.getBytes("UTF-8"));
             hash = Arrays.toString(digest);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return hash;
-    }
-
-}
+	} catch (UnsupportedEncodingException e) {
+	    e.printStackTrace();
+	} catch (NoSuchAlgorithmException e) {
+	    e.printStackTrace();
+	}
+	return hash;
+	}
+*/
